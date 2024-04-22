57d2f083e26fd1672849857a22def0be49460810
balex <balexvicx@gmail.com>
2024-04-22 14:47:45 +0200

    Row, Box

--- app/src/main/java/com/balex/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/balex/firstcomposeproject/MainActivity.kt
@@ -16,12 +16,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.balex.firstcomposeproject.ui.theme.InstagramProfileCard
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TimesTable()
+            //TimesTable()
+            InstagramProfileCard()
         }
     }
 }
