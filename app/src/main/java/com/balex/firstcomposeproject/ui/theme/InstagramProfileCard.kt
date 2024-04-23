d9c29c7599d380b8e96e310f3d5dd75a067019e1
balex <balexvicx@gmail.com>
2024-04-23 03:34:53 +0200

    Card

--- app/src/main/java/com/balex/firstcomposeproject/ui/theme/InstagramProfileCard.kt
+++ app/src/main/java/com/balex/firstcomposeproject/ui/theme/InstagramProfileCard.kt
@@ -1,5 +1,6 @@
 package com.balex.firstcomposeproject.ui.theme
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -9,6 +10,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -20,28 +24,44 @@ import androidx.compose.ui.unit.dp
 @Preview
 @Composable
 fun InstagramProfileCard() {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp),
+    Card(
+        shape = RoundedCornerShape(
+            topStart = 4.dp,
+            topEnd = 4.dp
+        ),
+        border = BorderStroke(1.dp, Color.Black),
+        colors = CardDefaults.cardColors(
+            //containerColor = Color.Green,
+            contentColor = Color.White
+        ),
 
-        horizontalArrangement = Arrangement.SpaceEvenly,
-        verticalAlignment = Alignment.CenterVertically
-    )
-    {
+        ) {
 
 
-        Box(
+        Row(
             modifier = Modifier
-                .size(50.dp)
-                .background(color = Color.Yellow)
+                .fillMaxWidth()
+                .padding(8.dp),
+
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
         )
+        {
 
-        TwoBoxes()
-        TwoBoxes()
-        TwoBoxes()
 
+            Box(
+                modifier = Modifier
+                    .size(50.dp)
+                    .background(color = Color.Yellow)
+            )
+
+            TwoBoxes()
+            TwoBoxes()
+            TwoBoxes()
+
+        }
     }
+
 }
 
 
