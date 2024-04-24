6e083f46b70f3795632e0199e11035fbd067603a
balex <balexvicx@gmail.com>
2024-04-24 14:09:33 +0200

    Modifiers order

--- app/src/main/java/com/balex/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/balex/firstcomposeproject/MainActivity.kt
@@ -7,13 +7,19 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.balex.firstcomposeproject.ui.theme.FirstComposeProjectTheme
 import com.balex.firstcomposeproject.ui.theme.InstagramProfileCard
 
@@ -36,15 +42,25 @@ class MainActivity : ComponentActivity() {
 
 @Preview
 @Composable
-fun TestImage() {
-    Image(
-        modifier = Modifier.clip(CircleShape),
-        //modifier = Modifier.fillMaxSize(),
-        painter = painterResource(id = R.drawable.ic_launcher_background),
-        contentDescription = "",
-        //contentScale = ContentScale.FillHeight
-        //contentScale = ContentScale.Fit
-    )
+fun TestText() {
+    Box(
+        modifier = Modifier
+            .size(200.dp)
+            .background(Color.Cyan)
+    ) {
+        Image(
+            modifier = Modifier
+                .background(Color.Green)
+                .padding(25.dp)
+                .clip(CircleShape)
+                .size(100.dp)
+                .background(Color.Red)
+                .padding(25.dp)
+                .clip(CircleShape)
+            ,
+            painter = ColorPainter(Color.Yellow),
+            contentDescription = "",
+            contentScale = ContentScale.FillHeight
+        )
+    }
 }
-
-
--- app/src/main/java/com/balex/firstcomposeproject/ui/theme/Theme.kt
+++ app/src/main/java/com/balex/firstcomposeproject/ui/theme/Theme.kt
@@ -33,8 +33,8 @@ private val LightColorPalette = lightColors(
 
 @Composable
 fun FirstComposeProjectTheme(
-    //darkTheme: Boolean = isSystemInDarkTheme(),
-    darkTheme: Boolean = !isSystemInDarkTheme(),
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    //darkTheme: Boolean = !isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
     val colors = if (darkTheme) {
