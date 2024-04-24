2f358f76ea3b9735e83a76ad04602655cbff295f
balex <balexvicx@gmail.com>
2024-04-24 03:28:13 +0200

    Text

--- app/src/main/java/com/balex/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/balex/firstcomposeproject/MainActivity.kt
@@ -3,11 +3,18 @@ package com.balex.firstcomposeproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.*
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.balex.firstcomposeproject.ui.theme.FirstComposeProjectTheme
 import com.balex.firstcomposeproject.ui.theme.InstagramProfileCard
 
@@ -27,3 +34,30 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+
+@Preview
+@Composable
+fun TestText() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colors.background)
+    ) {
+        Text(
+            buildAnnotatedString {
+                withStyle(SpanStyle(fontWeight = FontWeight.Bold, fontFamily = FontFamily.Cursive)) {
+                    append("Hello")
+                }
+                withStyle(SpanStyle(textDecoration = TextDecoration.Underline)) {
+                    append(" ")
+                }
+                withStyle(SpanStyle(fontSize = 30.sp, textDecoration = TextDecoration.LineThrough)) {
+                    append("World!")
+                }
+            }
+        )
+    }
+
+}
+
