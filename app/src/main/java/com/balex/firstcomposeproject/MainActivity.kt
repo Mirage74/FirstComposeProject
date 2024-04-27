93c2114e5ed03060aaa14f7f60124f3a517dbc92
balex <balexvicx@gmail.com>
2024-04-28 00:17:22 +0200

    Lazy column

--- app/src/main/java/com/balex/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/balex/firstcomposeproject/MainActivity.kt
@@ -3,11 +3,17 @@ package com.balex.firstcomposeproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.lifecycle.ViewModelProvider
 import com.balex.firstcomposeproject.ui.theme.FirstComposeProjectTheme
 import com.balex.firstcomposeproject.ui.theme.InstagramProfileCard
@@ -17,16 +23,36 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         val viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         setContent {
-            FirstComposeProjectTheme {
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(MaterialTheme.colors.background)
-                ) {
-                    InstagramProfileCard(viewModel)
+            Test(viewModel = viewModel)
+        }
+    }
+}
+
+    @Composable
+    private fun Test(viewModel: MainViewModel) {
+        FirstComposeProjectTheme {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(MaterialTheme.colors.background)
+            ) {
+                LazyColumn {
+                    item {
+                        Text(text = "Title", color = Color.Black)
+                    }
+                    items(10) {
+                        InstagramProfileCard(viewModel)
+                    }
+                    item {
+                        Image(painter = painterResource(id = R.drawable.ic_instagram), contentDescription = null)
+                    }
+                    items(50000) {
+                        //Log.d("it", it.toString())
+                        InstagramProfileCard(viewModel)
+                    }
                 }
             }
         }
-    }
+
 }
 
--- app/src/main/java/com/balex/firstcomposeproject/ui/theme/InstagramProfileCard.kt
+++ app/src/main/java/com/balex/firstcomposeproject/ui/theme/InstagramProfileCard.kt
@@ -1,6 +1,5 @@
 package com.balex.firstcomposeproject.ui.theme
 
-import android.util.Log
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -20,11 +19,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -42,16 +37,16 @@ import com.balex.firstcomposeproject.R
 fun InstagramProfileCard(
     viewModel: MainViewModel
 ) {
-    Log.d("RECOMPOSITION", "InstagramProfileCard")
+    //Log.d("RECOMPOSITION", "InstagramProfileCard")
     val isFollowed = viewModel.isFollowing.observeAsState(false)
     //val isFollowed by viewModel.isFollowing.observeAsState(false)
 
-    var a by remember {
-        mutableIntStateOf(5)
-    }
-
-    val b = a
-    a = 10
+//    var a by remember {
+//        mutableIntStateOf(5)
+//    }
+//
+//    val b = a
+//    a = 10
 
     Card(
         modifier = Modifier.padding(8.dp),
@@ -62,7 +57,7 @@ fun InstagramProfileCard(
         ),
         border = BorderStroke(1.dp, MaterialTheme.colors.onBackground)
     ) {
-        Log.d("RECOMPOSITION", "Card")
+        //Log.d("RECOMPOSITION", "Card")
         Column(
             modifier = Modifier.padding(16.dp)
         ) {
@@ -110,7 +105,7 @@ private fun FollowButton(
     isFollowed: State<Boolean>,
     clickListener: () -> Unit
 ) {
-    Log.d("RECOMPOSITION", "FollowButton")
+    //Log.d("RECOMPOSITION", "FollowButton")
     Button(
         onClick = { clickListener() },
         colors = ButtonDefaults.buttonColors(
