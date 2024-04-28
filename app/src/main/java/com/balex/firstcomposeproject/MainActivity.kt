dba7d2576178d3e0a2f2d41dd8921655d3701fd4
balex <balexvicx@gmail.com>
2024-04-28 02:26:25 +0200

    items(models.value, key = {it.id})

--- app/src/main/java/com/balex/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/balex/firstcomposeproject/MainActivity.kt
@@ -6,16 +6,29 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.DismissDirection
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.SwipeToDismiss
+import androidx.compose.material.Text
+import androidx.compose.material.rememberDismissState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModelProvider
 import com.balex.firstcomposeproject.ui.theme.FirstComposeProjectTheme
 import com.balex.firstcomposeproject.ui.theme.InstagramProfileCard
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,6 +40,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 private fun Test(viewModel: MainViewModel) {
     FirstComposeProjectTheme {
@@ -36,14 +50,46 @@ private fun Test(viewModel: MainViewModel) {
                 .background(MaterialTheme.colors.background)
         ) {
             val models = viewModel.models.observeAsState(listOf())
-            LazyVerticalGrid(columns = GridCells.Fixed(2)) {
-                items(models.value) { model ->
-                    InstagramProfileCard(
-                        model = model,
-                        onFollowedButtonClickListener = {
-                            viewModel.changeFollowingStatus(it)
+//            val lazyListState = rememberLazyListState()
+            LazyColumn(
+  //              state = lazyListState
+            ) {
+
+                items(models.value, key = {it.id}) { model ->
+                    val dismissState = rememberDismissState()
+
+                    if (dismissState.isDismissed(DismissDirection.EndToStart)) {
+                        viewModel.delete(model)
+                    }
+
+
+                    SwipeToDismiss(
+                        state = dismissState,
+                        directions = setOf(DismissDirection.EndToStart),
+                        background = {
+                            Box(
+                                modifier = Modifier
+                                    .padding(16.dp)
+                                    .fillMaxSize()
+                                    .background(Color.Red.copy(alpha = 0.5f)),
+                                contentAlignment = Alignment.CenterEnd
+                            ) {
+                                Text(
+                                    modifier = Modifier.padding(16.dp),
+                                    text = "Delete item",
+                                    color = Color.White,
+                                    fontSize = 24.sp
+                                )
+                            }
                         }
-                    )
+                    ) {
+                        InstagramProfileCard(
+                            model = model,
+                            onFollowedButtonClickListener = {
+                                viewModel.changeFollowingStatus(it)
+                            }
+                        )
+                    }
                 }
             }
         }
